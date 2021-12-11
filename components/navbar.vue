b22799826cfd527c674f4b8b78c271817512121c
Lukasz Kolano <lukaszkolano28@gmail.com>
2021-12-11 15:08:48 +0000

    packages update v0.1

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,7 +11,7 @@
                     <NuxtLink to="/">Home</NuxtLink>
                     <NuxtLink to="/gear">My Gear</NuxtLink>
                     <NuxtLink to="/gallery">My work</NuxtLink>
-                    <NuxtLink to="/shoots">Packages</NuxtLink>
+                    <NuxtLink to="/packages">Packages</NuxtLink>
                     <NuxtLink to="/contact">Contact</NuxtLink>
                 </nav>
             </div>
