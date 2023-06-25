c98d897bee539bf512fc1ff616eb42e905f13946
Lukasz Kolano <lukaszkolano28@gmail.com>
2023-06-25 17:52:11 +0100

    packages

--- pages/packages.vue
+++ pages/packages.vue
@@ -8,15 +8,132 @@
             </div>
             <div class="shoots--content">
                 <div class="shoots--content-box">
-                    <h2 class="fadeIn-2">Package GT</h2>
-                    <p class="fadeIn">coming soon</p>
+                    <h2 class="fadeIn-2">
+                        "Economy Auto Bundle"
+                        <p>
+                            <a
+                                href="https://www.instagram.com/clashofcars.dev/ "
+                                target="_blank"
+                                >Ask for price*</a
+                            >
+                        </p>
+                    </h2>
+                    <div class="fadeIn bundle-desc">
+                        <p>
+                            - 1-hour outdoor photoshoot session at a single
+                            location.
+                        </p>
+                        <p>
+                            - 10 high-resolution edited images of the client's
+                            preferred vehicles.
+                        </p>
+                        <p>
+                            - Basic retouching and color correction for a clean
+                            and presentable look.
+                        </p>
+
+                        <p>
+                            - A 30-second highlight video showcasing the
+                            vehicles in motion.
+                        </p>
+                        <p>
+                            - Online gallery for easy image and video sharing
+                            and downloading.
+                        </p>
+                    </div>
                 </div>
                 <div class="shoots--content-box">
-                    <h2 class="fadeIn-2">Package GTX</h2>
-                    <p class="fadeIn">coming soon</p>
+                    <h2 class="fadeIn-2">
+                        "Value Auto Package"
+                        <p>
+                            <a
+                                href="https://www.instagram.com/clashofcars.dev/ "
+                                target="_blank"
+                                >Ask for price*</a
+                            >
+                        </p>
+                    </h2>
+                    <div class="fadeIn bundle-desc">
+                        <p>
+                            - 2-hour on-location photoshoot session at up to two
+                            locations.
+                        </p>
+                        <p>
+                            - 20 high-resolution edited images of the client's
+                            preferred vehicles.
+                        </p>
+                        <p>
+                            - Advanced retouching, color grading, and
+                            enhancement for a professional finish.
+                        </p>
+                        <p>
+                            - A 1-minute promotional video featuring the
+                            vehicles in different settings.
+                        </p>
+                        <p>
+                            - Detail shots capturing unique features and
+                            close-ups.
+                        </p>
+                        <p>
+                            - Online gallery for easy image and video sharing
+                            and downloading.
+                        </p>
+                    </div>
+                </div>
+                <div class="shoots--content-box">
+                    <h2 class="fadeIn-2">
+                        "Premium Auto Collection"
+                        <p>
+                            <a
+                                href="https://www.instagram.com/clashofcars.dev/ "
+                                target="_blank"
+                                >Ask for price*</a
+                            >
+                        </p>
+                    </h2>
+                    <div class="fadeIn bundle-desc">
+                        <p>
+                            - Half-day (4-hour) on-location photoshoot session
+                            at multiple desired locations.
+                        </p>
+                        <p>
+                            - Up to three vehicles photographed in different
+                            settings.
+                        </p>
+                        <p>
+                            - 40 high-resolution edited images of the client's
+                            preferred vehicles. - Extensive retouching, advanced
+                            color grading, and enhancement for a premium look.
+                        </p>
+                        <p>
+                            - A 2-minute cinematic video highlighting the
+                            vehicles' beauty and performance.
+                        </p>
+                        <p>
+                            - Detail shots showcasing intricate details and
+                            craftsmanship.
+                        </p>
+                        <p>
+                            - Online gallery for easy image and video sharing
+                            and downloading.
+                        </p>
+                        <p>
+                            - One 11x14 fine art print of the client's favorite
+                            image.
+                        </p>
+                    </div>
                 </div>
             </div>
+            <div class="disclamer">
+                <p>
+                    *Please note that the prices mentioned are for
+                    illustrative purposes and can vary based on various factors.
+                    It's recommended to consult with the photographer directly
+                    for accurate pricing and customization options.
+                </p>
+            </div>
         </div>
+
         <foot />
     </div>
 </template>
